3982f9a7a8e77b40c3500b02b40f52f542b69d89
JasmineAlves <jasmine_alves@estudante.sesisenai.org.br>
2023-07-27 21:25:51 -0300

    #atualizado da última aula, tentativa falha de conexão com banco de dados

---
+++ appwebsenai/src/main/java/com/example/appwebsenai/controller/PersonRepository.java
@@ -0,0 +1,15 @@
+package com.example.appwebsenai.controller;
+
+import com.example.appwebsenai.model.Person;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface PersonRepository extends CrudRepository <Person, Integer> {
+
+    List<Person> findAll();
+
+    void save(Person person);
+
+    void delete(Person person);
+}
