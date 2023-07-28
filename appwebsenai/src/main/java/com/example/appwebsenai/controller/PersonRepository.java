b5ccbe173ce73e2522c24a6e4900335f6d872dfa
JasmineAlves <jasmine_alves@estudante.sesisenai.org.br>
2023-07-27 21:58:42 -0300

    # Atualizado, banco h2

--- appwebsenai/src/main/java/com/example/appwebsenai/controller/PersonRepository.java
+++ appwebsenai/src/main/java/com/example/appwebsenai/controller/PersonRepository.java
@@ -5,11 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface PersonRepository extends CrudRepository <Person, Integer> {
+public interface PersonRepository extends CrudRepository < Person, Integer > {
 
-    List<Person> findAll();
-
-    void save(Person person);
-
-    void delete(Person person);
 }
